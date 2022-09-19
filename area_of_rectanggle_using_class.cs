73664a705e2fff9562179ffa38a02c791b6cc5a8
ashish200214 <64855416+ashish200214@users.noreply.github.com>
2022-09-19 22:53:31 +0530

    Update area_of_rectanggle_using_class.cs

--- area_of_rectanggle_using_class.cs
+++ area_of_rectanggle_using_class.cs
@@ -1,34 +1,23 @@
 using System;
-namespace Rectangleapp{
-
-    class Main{
-        double length;
-        double width;
-        public void Acceptdetails()
+namespace areaofrectangle{
+    class areaofrec{
+        public static void area(int length,int breadth)
         {
-            length=4.5;
-            width=3.5;
+            int area;
+            area=length*breadth;
+            Console.WriteLine("area of rectangle is "+area);
         }
-        public double GetArea()
-        {
-            return length*width;
-        }
-        public void Display()
-        {
-            Console.WriteLine("Length = {0}",length);
-            Console.WriteLine("breadth = {0}",width);
-            Console.WriteLine("Area is = {0}",GetArea());
-        }
-    }
-    class ExecuteRectangle
-    {
-        static void Main (String[]args)
-        {
-        Main r=new Main();
-        r.Acceptdetails();
-        r.Display();
+        class main{
+            static void Main(String[]args)
+            {
+                int length,breadth;
+                Console.WriteLine("enter the length");
+                length=int.Parse(Console.ReadLine());
+                Console.WriteLine("enter the breadth");
+                breadth=int.Parse(Console.ReadLine());
+                area(length,breadth);
+            }
         }
         
     }
 }
-
